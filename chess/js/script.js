33d4ba75f191aeaaac601a4c727133c0e6daf7ef
Tanya Fortuna <tanyafortuna24@gmail.com>
2025-07-19 18:04:35 -0600

    Refactor change theme function

--- chess/js/script.js
+++ chess/js/script.js
@@ -4,6 +4,7 @@ let numbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
 let tempHTML = "";
 let bgColorMarker = 0;
 let themeMarker = 0;
+let themes = ["blue", "browns", "greens", "pinks"];
 
 // Elements
 let boardDiv = document.getElementById("chessboard");
@@ -25,8 +26,8 @@ for (let i = 0; i < letters.length; i++) {
   for (let n = 0; n < numbers.length; n++) {
     tempHTML += "<div id='" + letters[i] + numbers[n] + "'";
 
-    if (bgColorMarker % 2 == 0) { tempHTML += " class='light'"; }
-    else { tempHTML += " class='dark'"; }
+    if (bgColorMarker % 2 == 0) { tempHTML += " class='light blue'"; }
+    else { tempHTML += " class='dark blue'"; }
     bgColorMarker++;
 
     tempHTML += "></div>";
@@ -51,41 +52,13 @@ function rotateBoard() {
 }
 
 function changeTheme() {
-  let lightDivs = document.querySelectorAll("div.light");
-  let darkDivs = document.querySelectorAll("div.dark");
+  let divs = document.querySelectorAll("div.light, div.dark");
 
-  switch (themeMarker) {
-    case 0:
-      for (let div of lightDivs) { div.classList.add("browns"); }
-      for (let div of darkDivs) { div.classList.add("browns"); }
-      themeMarker++;
-      break;
-    case 1:
-      for (let div of lightDivs) {
-        div.classList.remove("browns");
-        div.classList.add("greens");
-      }
-      for (let div of darkDivs) {
-        div.classList.remove("browns");
-        div.classList.add("greens");
-      }
-      themeMarker++;
-      break;
-    case 2:
-      for (let div of lightDivs) {
-        div.classList.remove("greens");
-        div.classList.add("pinks");
-      }
-      for (let div of darkDivs) {
-        div.classList.remove("greens");
-        div.classList.add("pinks");
-      }
-      themeMarker++;
-      break;
-    case 3:
-      for (let div of lightDivs) { div.classList.remove("pinks"); }
-      for (let div of darkDivs) { div.classList.remove("pinks"); }
-      themeMarker = 0;
-      break;
+  for (let div of divs) {
+    div.classList.remove(themes[themeMarker]);
+    div.classList.add(themes[(themeMarker + 1) % 4]);
   }
+
+  themeMarker++;
+  if (themeMarker > 3) themeMarker = 0;
 }
